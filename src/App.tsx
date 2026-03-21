454e78efef9ee0015b2a47731a798acd6e7d05a5
clestbest <clestbest@gmail.com>
2026-03-21 10:20:57 -0600

    Fix build errors, update local debug config, change homepage message

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function App() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', margin: 0 }}>
-      <h1>Celeste Whelan Resume</h1>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        margin: 0,
+      }}
+    >
+      <h1>Celeste Whelan Resume + Typescript</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
